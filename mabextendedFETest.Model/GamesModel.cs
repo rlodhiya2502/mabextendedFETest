5ded78702b1bc2631c2446c11fc94f9a089db223
rlodhiya2502 <67738483+rlodhiya2502@users.noreply.github.com>
2022-09-22 11:16:20 +0100

    Add Model Project

---
+++ mabextendedFETest.Model/GamesModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace mabextendedFETest.Model
+{
+    public class GamesModel
+    {
+        public int gamesid { get; set; }
+        [Range(int.MinValue, int.MaxValue)]
+        public int playerid { get; set; }
+        [Range(int.MinValue, int.MaxValue)]
+        public int score { get; set; }
+        [Required]
+        public DateTime gamedatetime { get; set; }
+    }
+}
+}
---
+++ mabextendedFETest.Model/PlayersModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace mabextendedFETest.Model
+{
+    public class PlayersModel
+    {
+        public int playerid { get; set; }
+        [Required]
+        public string name { get; set; }
+    }
+}
